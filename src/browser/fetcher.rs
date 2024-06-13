f55714f82b35eeda304a37b56cb06803bd182fd1
mrfoxpro <tuningiposadka@gmail.com>
2024-06-13 19:39:13 +0500

    improvements

--- src/browser/fetcher.rs
+++ src/browser/fetcher.rs
@@ -220,7 +220,7 @@ impl Fetcher {
         )
         .map_err(|_err| anyhow!("Could not create directory at {:?}", path.parent()))?;
 
-        println!("{path:?}");
+      	info!("downloading chromium: {path:?}");
 
         info!("Creating file for download: {}", &path.display());
         let mut file = OpenOptions::new()
