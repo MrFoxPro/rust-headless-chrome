d5c684c91e3e37705be19cf0539def5024b85978
mrfoxpro <tuningiposadka@gmail.com>
2024-06-15 20:28:10 +0500

    da

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -325,8 +325,8 @@ impl Process {
 
         let mut args = vec![
             port_option.as_str(),
-            "--verbose",
-            "--log-level=0",
+            // "--verbose",
+            // "--log-level=0",
             "--no-first-run",
             data_dir_option.as_str(),
         ];
